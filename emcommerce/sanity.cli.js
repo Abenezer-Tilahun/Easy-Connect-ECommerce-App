f4d730e39c1adc2cf1c83fabaab8671f014a1271
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2024-02-06 16:57:57 +0300

    Fix the linter

--- emcommerce/sanity.cli.js
+++ emcommerce/sanity.cli.js
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
     projectId: '4uma0zt0',
-    dataset: 'production'
-  }
-})
+    dataset: 'production',
+  },
+});
